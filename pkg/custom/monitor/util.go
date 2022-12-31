f63cca962b9de69ca940b80b17f6ea3cea4406d8
0yukali0 <a08h0283@gmail.com>
2022-12-31 13:26:11 +0800

    basic200

--- pkg/custom/monitor/util.go
+++ pkg/custom/monitor/util.go
@@ -14,7 +14,7 @@ const (
 	utilizationfiltpath = "/tmp/utiliztion.xlsx"
 	tenantsfiltpath     = "/tmp/tenants.xlsx"
 	fairness            = "tenants"
-	appNum              = 16
+	appNum              = 200
 )
 
 var excelCol []string = []string{"B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
