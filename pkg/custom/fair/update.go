a5a94a1045e2a7660247600cfd718a041e935a80
0yukali0 <a08h0283@gmail.com>
2023-07-16 08:35:17 +0800

    fix grammer

--- pkg/custom/fair/update.go
+++ pkg/custom/fair/update.go
@@ -74,7 +74,7 @@ func (f *FairManager) RemoveRunningApp(appID string) {
 
 func (f *FairManager) AddCompletedApp(input *objects.Application) {
 	appID, user, _ := util.ParseApp(input)
-	res := resources.ParseAppWithoutDuration(input)
+	res := util.ParseAppWithoutDuration(input)
 	if _, ok := f.completedApps[appID]; !ok {
 		f.completedApps[appID] = NewAppInfo(user, res)
 	}
